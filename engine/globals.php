a2539b2ab9ca9307716e4fb4d2cc0ab51e9d8e44
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-02 16:19:19 +0200

    Minor changes to the main file

--- engine/globals.php
+++ engine/globals.php
@@ -1,25 +1,26 @@
 <?php
 
-ob_start(); 
+declare(strict_types=1);
 
-# Встановлює заголовки HTTP
+ob_start();
+
+# Встановлення заголовків HTTP
 header('Powered: LiveCMS');
-header("Cache-control: public");
-header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60 * 60 * 24) . " GMT");
+header('Cache-control: public');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 60 * 60 * 24) . ' GMT');
 
 # Запуск сесій
-@session_name('SID');
-@session_start();
+session_name('SID');
+session_start();
 
-$sessID = addslashes(session_id());
-
-if (!preg_match('#[A-z0-9]{32}#i', $sessID)) {
-  $sessID = md5(mt_rand(000000, 999999));
+$sessID = session_id();
+if (!preg_match('#[A-Za-z0-9]{32}#i', $sessID)) {
+    $sessID = md5((string) random_int(100000, 999999));
 }
 
 /**
  * Видаляє потенційно небезпечні скрипти та символи зі строки.
- * 
+ *
  * @param string|null $string Вхідний рядок для обробки.
  * @return string Очищений рядок.
  */
@@ -28,14 +29,16 @@ function remove_script(?string $string = null): string {
     if ($string === null) {
         return '';
     }
-    # Видалення неприпустимих символів (NULL, керуючі символи та DEL).
-    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string);
-    # Список потенційно небезпечних тегів і атрибутів.
+
+    // Видалення неприпустимих символів (NULL, керуючі символи та DEL).
+    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/u', '', $string);
+
+    // Чорний список небезпечних тегів та атрибутів
     $blacklist = [
-        # Небезпечні теги.
+        // Небезпечні теги.
         'vbscript', 'expression', 'applet', 'xml', 'blink', 'embed', 'object', 
         'frameset', 'ilayer', 'layer', 'bgsound',
-        # Обробники подій.
+        // Обробники подій.
         'onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 
         'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 
         'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 
@@ -53,29 +56,25 @@ function remove_script(?string $string = null): string {
         'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 
         'onstart', 'onstop', 'onsubmit', 'onunload'
     ];
-    # Видалення ключових слів із рядка.
+
     foreach ($blacklist as $keyword) {
-        # Генеруємо регулярний вираз із врахуванням можливих роздільників.
-        $pattern = '/';
-        foreach (str_split($keyword) as $char) {
-            $pattern .= preg_quote($char, '/') . '(&#[xX]0(?:9|a|b);?|&#(?:9|10|13);?)?';
-        }
-        $pattern .= '/i';
-        # Замінюємо знайдені ключові слова на пробіл.
-        $string = preg_replace($pattern, ' ', $string);
+        $string = preg_replace('/' . preg_quote($keyword, '/') . '/iu', '', $string);
     }
+
     return $string;
 }
 
 /**
  * Фільтрує рядок для безпечного виведення.
- * 
+ *
  * @param string $data Вхідний рядок.
  * @return string Очищений рядок.
  */
 
 function _filter(string $data): string {
-    return remove_script(addslashes(htmlspecialchars($data, ENT_QUOTES | ENT_HTML5)));
+    return htmlspecialchars(remove_script($data), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+}
+
 #  Поточний системний час
 define('TM', time());
 
