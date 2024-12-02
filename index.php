2c273f5f30a89af6c441be1b689d07845afdee75
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-02 15:51:37 +0200

    The main project launch file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SECURITY_LIVECMS', true);
+define('ROOT', dirname(__FILE__));
+define('LCMS', ROOT . '/engine');
+
+require_once(LCMS . '/globals.php');
+require_once(LCMS . '/livecms.php');
