15de28b611b5e9bdfea452caf8505ee6fd6863ca
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-02 18:58:46 +0200

    Class "direct " - Data filtering from a GET request.

--- engine/classes/direct.class.php
+++ engine/classes/direct.class.php
@@ -6,4 +6,22 @@
 
 class direct {
 
+    /**
+     * Фільтрація даних з GET-запиту.
+     *
+     * @param string $get_name Назва GET-параметра.
+     * @return string Повертає очищене значення GET-параметра або 'no_data', якщо дані відсутні.
+     */
+
+    public static function get($name) {
+        // Фільтруємо вхідні дані з GET-параметра
+        $filter = filter_input(INPUT_GET, $name, FILTER_SANITIZE_ENCODED);
+        // Видаляємо зайві спецсимволи, повернуті функцією FILTER_SANITIZE_ENCODED
+        $get = clearspecialchars($filter);
+        // Перевіряємо, чи довжина отриманого значення більша за нуль.
+        $data = (strlen($get) > 0) ? $get : 'no_data';
+        // Повертаємо значення змінної $data
+        return $data;
+    }
+
 }
