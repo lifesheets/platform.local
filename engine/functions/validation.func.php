36e0180c409743f46f2d0754b64e662f0b382443
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-03 08:40:03 +0200

    Update PHP 8.4

--- engine/functions/validation.func.php
+++ engine/functions/validation.func.php
@@ -12,17 +12,19 @@
  * @return string Повертає текст без спеціальних символів.
  */
 
-function clearspecialchars($text) {
-    // Масив спеціальних символів, які потрібно видалити з тексту.
+ function clearspecialchars($text) {
+    // Проверка, является ли $text строкой, и обработка null
+    if (!is_string($text) || $text === null) : $text = ''; endif;
+    // Массив специальных символов, которые нужно удалить из текста
     $special_chars = array('?', '[', ']', '/', '\\', '=', '<', '>', ':', ';', ',', "'", '"', '&', '$', '#', '*', '(', ')', '|', '~', '`', '!', '{', '}', '%', '+', chr(0));
-    // Заміна символу неперервного пробілу на звичайний пробіл.
-    $text = preg_replace("#\x{00a0}#siu", ' ', $text);
-    // Видалення спеціальних символів.
-    $text = str_replace($special_chars, '', $text);
-    // Видалення '%20' і '+' (пробіли в URL).
-    $text = str_replace(array('%20', '+'), '', $text);
-    // Обрізання крапок, дефісів і підкреслень на початку та в кінці тексту.
-    $text = trim($text, '.-_');
-    // Повертає текст, оброблений функцією `htmlspecialchars()` для запобігання XSS-атакам.
+    // Замена символа неразрывного пробела на обычный пробел
+    if (preg_match("#\x{00a0}#siu", $text)) : $text = preg_replace("#\x{00a0}#siu", ' ', $text); endif;
+    // Удаление специальных символов
+    if (strpbrk($text, implode('', $special_chars)) !== false) : $text = str_replace($special_chars, '', $text); endif;
+    // Удаление '%20' и '+' (пробелы в URL)
+    if (strpos($text, '%20') !== false || strpos($text, '+') !== false) : $text = str_replace(array('%20', '+'), '', $text); endif;
+    // Обрезание точек, дефисов и подчеркиваний в начале и конце текста
+    if ($text !== '') : $text = trim($text, '.-_'); endif;
+    // Возвращение текста, обработанного функцией `htmlspecialchars()` для предотвращения XSS-атак
     return htmlspecialchars($text);
 }
